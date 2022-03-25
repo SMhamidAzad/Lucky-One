d297db5322955d28e58858e74199ef06205182f7
SMhamidAzad <hamidazad020@gmail.com>
2022-03-25 22:51:47 +0600

    display all info in ui

--- src/Components/DisplayCar/DisplayCar.js
+++ src/Components/DisplayCar/DisplayCar.js
@@ -8,8 +8,9 @@ const SingleCar = (props) => {
             <img src={props.car.img} alt="" className="card-img-top" />
           {/* <img src="..." className="card-img-top" alt="..."> */}
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 className="card-title">{props.car.name}</h5>
+            <p className="card-text">Price: ${props.car.price}</p>
+            <button className='btn btn-success'>Add to Card</button>
           </div>
         </div>
       </div>
