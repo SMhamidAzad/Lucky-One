b9696caafab15a707d89fe59c79679db87a60941
SMhamidAzad <hamidazad020@gmail.com>
2022-03-25 22:47:13 +0600

    Display img in ui

--- src/Components/Car/Car.js
+++ src/Components/Car/Car.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
+import DisplayCar from '../DisplayCar/DisplayCar';
 
 const Car = () => {
-    const [car,setCar]=useState([]);
+    const [cars,setCars]=useState([]);
     useEffect(()=>{
         fetch('data.json')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setCars(data))
     },[])
     return (
-        <div>
-            
+        <div className='row'>
+            <div className='col-md-9'>
+                <div className='row row-cols-1 row-cols-md-3 g-4'>
+                {
+                 cars.map(item => <DisplayCar car={item}></DisplayCar>) 
+                }
+                </div>
+            </div>
+            <div className='col-md-3'>
+                 <Card></Card>
+            </div>
         </div>
     );
 };
